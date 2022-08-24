ef075a77d97ed503ab5613d007f2da944c1b836f
Mateo <mateom.realtor@gmail.com>
2022-08-24 09:39:39 -0400

    Completed service page

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ let menuBtn = document.getElementsByClassName("navbar-toggler");
 menuBtn[0].addEventListener('click',()=>{
     console.log("heuuu")
 })
+
+let serviceSectionCol = document.getElementsByClassName("col-sm")
+
+// Looping through all the columns in the service section page 
+// for (let i = 0; i < serviceSectionCol.length; i++) {
+//     serviceSectionCol[i].addEventListener('click',()=>{
+//     console.log("heyyyyy")
+//     })
+// }
+ 
